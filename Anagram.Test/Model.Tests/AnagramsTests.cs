e75c8a781efbc8ef872aa3337a5761ae005e72ab
Kelli Brooke and Matt Smith <kelli@>
2018-06-27 15:07:46 -0700

    added letter parsing and string conversion to array

--- Anagram.Test/Model.Tests/AnagramsTests.cs
+++ Anagram.Test/Model.Tests/AnagramsTests.cs
@@ -1,13 +1,59 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Anagranimals.Models;
+using Anagranimals;
 
-namespace Anagranimals.Tests.Model.Tests
+namespace Anagranimals.Tests
 {
-    public class AnagramsTests
+    [TestClass]
+    public class AnagramTest
     {
-        public AnagramsTests()
+        [TestMethod]
+        public void IsPartOfAlphabet_CheckIfLetter_Bool()
         {
+            char test = 's';
+            Anagram newAnalgram = new Anagram("any string");
+            bool answer = newAnalgram.IsPartOfAlphabet(test);
+            Assert.AreEqual(answer, true);
+        }
+
+        [TestMethod]
+        public void TestArrayLength_FindLengthOfArray_Int()
+        {
+            char[] testArray = { 't', 'e', 's', 't' };
+            Anagram newAnalgram = new Anagram("any string");
+            int length = newAnalgram.TestArrayLength(testArray);
+            Assert.AreEqual(length, 4);
+        }
+
+        [TestMethod]
+        public void StringToArray_StringConvertedToCharArray_CharArray()
+        {
+            string word = "test";
+            char[] charArray = { 't', 'e', 's', 't' };
+            Anagram newAnalgram = new Anagram(word);
+            char[] methodCharArray = newAnalgram.StringToArray(word);
+            CollectionAssert.AreEqual(methodCharArray, charArray);
+        }
+
+        [TestMethod]
+        public void SetOriginalWord_TestSetOGWord_String()
+        {
+            string word = "catch";
+            Anagram newAnalgram = new Anagram(word);
+            string anotherWord = "hatch";
+            newAnalgram.SetOriginalWord(anotherWord);
+            string yetAnotherWord = newAnalgram.GetOriginalWord();
+            Assert.AreEqual(anotherWord, yetAnotherWord);
+        }
+
+        [TestMethod]
+        public void GetOriginalWord_TestGettingOriginalWord_String()
+        {
+            string word = "catch";
+            Anagram newAnagram = new Anagram(word);
+            string actualWord = newAnagram.GetOriginalWord();
+            Assert.AreEqual(actualWord, word);
         }
     }
 }
+ 
--- Anagranimals/Models/Anagrams.cs
+++ Anagranimals/Models/Anagrams.cs
@@ -1,10 +1,47 @@
-﻿using System;
-namespace Anagranimals.Models
+﻿using System.Collections.Generic;
+
+namespace Anagranimals
 {
-    public class Anagrams
+    public class Anagram
     {
-        public Anagrams()
+        private string _originalWord;
+        //private string _wordToCompare;
+
+        public Anagram(string originalWord)
         {
+            _originalWord = originalWord;
         }
+
+        public string GetOriginalWord()
+        {
+            return _originalWord;
+        }
+
+        public void SetOriginalWord(string newWord)
+        {
+            _originalWord = newWord;
+        }
+
+        public char[] StringToArray(string word)
+        {
+            char[] newArray = word.ToCharArray();
+            return newArray;
+        }
+
+        public bool IsPartOfAlphabet(char letter)
+        {
+            bool answer = char.IsLetter(letter);
+            return answer;
+        }
+
+        public int TestArrayLength(char[] array)
+        {
+            int length = array.Length;
+            return length;
+        }
+
+
+
+
     }
 }
